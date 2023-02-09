d2ce7928b93a7fefa596ea5c1ae30a153a00ab59
Guilherme Romão <guilhermersimoes87@gmail.com>
2023-02-09 11:22:59 -0300

    aula19

--- src/main/java/br/com/guiromao/restaurante/controller/HomeController.java
+++ src/main/java/br/com/guiromao/restaurante/controller/HomeController.java
@@ -2,28 +2,45 @@ package br.com.guiromao.restaurante.controller;
 
 import br.com.guiromao.restaurante.model.Categoria;
 import br.com.guiromao.restaurante.model.Produto;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
     @GetMapping
-    public String home() {
+    public ModelAndView home() {
 
-        Produto produto = new Produto();
-        produto.setId(1);
-        produto.setNome("Salada");
-        produto.setDescricao("Salada de alface e tomate");
-        produto.setCategoria(Categoria.COMIDA);
-        produto.setPreco(new BigDecimal(35));
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("produto", new Produto(
+                1,
+                "Salada",
+                "Salada de Alface e tomate",
+                Categoria.COMIDA,
+                BigDecimal.TEN
+        ));
 
-        return "home";
+        return modelAndView;
+    }
+
+    @GetMapping("/lista")
+    public String lista(Model model) {
+        Produto p1 = new Produto(1, "Salada", "Salada de Alface e tomate", Categoria.COMIDA, BigDecimal.TEN);
+        Produto p2 = new Produto(2, "Frango grelhado", "Acompanha arros", Categoria.COMIDA, BigDecimal.ONE);
+        Produto p3 = new Produto(3, "Refrigerante", "Aguá tonica sem açucar", Categoria.BEBIDA, BigDecimal.ZERO);
+
+        model.addAttribute("produtos", Arrays.asList(p1, p2, p3));
+
+        System.out.println(Arrays.asList(p1, p2, p3));
+
+        return "produtos";
     }
 }
--- src/main/java/br/com/guiromao/restaurante/model/Produto.java
+++ src/main/java/br/com/guiromao/restaurante/model/Produto.java
@@ -10,23 +10,32 @@ public class Produto {
     private Categoria categoria;
     private BigDecimal preco;
 
-    public void setId(Integer id) { this.id = id; }
+    public Produto(Integer id, String nome, String descricao, Categoria categoria, BigDecimal preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
 
     public Integer getId() { return id; }
 
-    public void setNome(String nome) { this.nome = nome; }
-
     public String getNome() { return nome; }
-
-    public void setDescricao(String descricao) { this.descricao = descricao; }
 
     public String getDescricao() { return descricao; }
 
-    public void setCategoria(Categoria categoria) { this.categoria = categoria; }
-
     public Categoria getCategoria() { return categoria; }
 
-    public void setPreco(BigDecimal preco) { this.preco = preco; }
-
     public BigDecimal getPreco() { return preco; }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", categoria=" + categoria +
+                ", preco=" + preco +
+                '}';
+    }
 }
