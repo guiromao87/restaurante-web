c6cb4c5b8707cdc7ab98da12a9a7d8b78f00b7e2
Guilherme Romão <guilhermersimoes87@gmail.com>
2023-03-18 01:58:53 -0300

    aula 30 - in memory

--- src/main/java/br/com/guiromao/restaurante/config/SecurityConfiguration.java
+++ src/main/java/br/com/guiromao/restaurante/config/SecurityConfiguration.java
@@ -1,9 +1,12 @@
 package br.com.guiromao.restaurante.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -11,5 +14,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests().anyRequest().authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("gui")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
